8c7abf371408c15bd0f8fce4e80fc9b935ff7f13
Dmitry Volodin <dv@nocproject.org>
2023-01-19 11:16:52 +0100

    Fix typo

--- examples/getmany.py
+++ examples/getmany.py
@@ -6,8 +6,8 @@ from gufo.snmp import SnmpSession
 
 async def main(addr: str, community: str, oids: List[str]) -> None:
     async with SnmpSession(addr=addr, community=community) as session:
-        r = await session.get_many(oid)
-        for k, v in r.items()
+        r = await session.get_many(oids)
+        for k, v in r.items():
             print(f"{k}: {v}")
 
 
