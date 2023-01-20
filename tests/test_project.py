f928729e37549d5c8157699cbd87d429e37d414f
Dmitry Volodin <dv@nocproject.org>
2023-01-20 13:28:01 +0100

    Gufo Labs project structure tests

---
+++ tests/test_project.py
@@ -0,0 +1,88 @@
+# ---------------------------------------------------------------------
+# Gufo Labs: Project structure tests
+# ---------------------------------------------------------------------
+# Copyright (C) 2022, Gufo Labs
+# See LICENSE.md for details
+# ---------------------------------------------------------------------
+
+# Python modules
+import os
+import sys
+import inspect
+
+# Third-party modules
+import pytest
+
+
+def _get_root():
+    mod_path = inspect.getfile(sys.modules[__name__])
+    rel_root = os.path.dirname(mod_path)
+    return os.path.abspath(os.path.join(rel_root, ".."))
+
+
+def _get_project():
+    d = [
+        f
+        for f in os.listdir(os.path.join(ROOT, "src", "gufo"))
+        if not f.startswith(".") and not f.startswith("_")
+    ]
+    assert len(d) == 1
+    return d[0]
+
+
+ROOT = _get_root()
+PROJECT = _get_project()
+
+REQUIRED_FILES = [
+    ".devcontainer/devcontainer.json",
+    ".github/CODEOWNERS",
+    ".github/ISSUE_TEMPLATE/bug-report.yml",
+    ".github/ISSUE_TEMPLATE/feature-request.yml",
+    ".github/PULL_REQUEST_TEMPLATE.md",
+    ".gitignore",
+    ".requirements/docs.txt",
+    ".requirements/lint.txt",
+    ".requirements/test.txt",
+    "CHANGELOG.md",
+    "CITATION.cff",
+    "CODE_OF_CONDUCT.md",
+    "CONTRIBUTING.md",
+    "Dockerfile",
+    "LICENSE.md",
+    "README.md",
+    "SECURITY.md",
+    "docs/assets/logo.png",
+    "docs/codebase.md",
+    "docs/codequality.md",
+    "docs/devcommon.md",
+    "docs/environment.md",
+    "docs/examples/index.md",
+    "docs/faq.md",
+    "docs/index.md",
+    "docs/installation.md",
+    "docs/testing.md",
+    "mkdocs.yml",
+    "pyproject.toml",
+    "setup.cfg",
+    f"src/gufo/{PROJECT}/__init__.py",
+    f"src/gufo/{PROJECT}/py.typed",
+    "tests/test_docs.py",
+    "tests/test_project.py",
+]
+
+
+def test_required_is_sorted():
+    assert REQUIRED_FILES == list(
+        sorted(REQUIRED_FILES)
+    ), "REQUIRED_FILES must be sorted"
+
+
+@pytest.mark.parametrize("name", REQUIRED_FILES)
+def test_required_files(name: str):
+    full_path = os.path.join(ROOT, name)
+    assert os.path.exists(full_path), f"File {name} is missed"
+
+
+def test_version():
+    m = __import__(f"gufo.{PROJECT}", {}, {}, "*")
+    assert hasattr(m, "__version__"), "__init__.py must contain __version__"
