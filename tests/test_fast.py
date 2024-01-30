4fb060bcbfdff6370aa698bda08981229fcf34e6
Dmitry Volodin <dv@nocproject.org>
2024-01-30 18:23:13 +0100

    Fix tests

--- tests/test_fast.py
+++ tests/test_fast.py
@@ -14,35 +14,35 @@ from gufo.snmp._fast import SnmpV1ClientSocket, SnmpV2cClientSocket
 
 def test_v1_invalid_address() -> None:
     with pytest.raises(OSError, match="invalid address"):
-        SnmpV1ClientSocket("127.0.0.500:161", "public", 0, 0, 0)
+        SnmpV1ClientSocket("127.0.0.500:161", "public", 0, 0, 0, 0)
 
 
 def test_v2c_invalid_address() -> None:
     with pytest.raises(OSError, match="invalid address"):
-        SnmpV2cClientSocket("127.0.0.500:161", "public", 0, 0, 0)
+        SnmpV2cClientSocket("127.0.0.500:161", "public", 0, 0, 0, 0)
 
 
 def test_v1_invalid_port() -> None:
     with pytest.raises(OSError, match="invalid address"):
-        SnmpV1ClientSocket("127.0.0.1:100000", "public", 0, 0, 0)
+        SnmpV1ClientSocket("127.0.0.1:100000", "public", 0, 0, 0, 0)
 
 
 def test_v2c_invalid_port() -> None:
     with pytest.raises(OSError, match="invalid address"):
-        SnmpV2cClientSocket("127.0.0.1:100000", "public", 0, 0, 0)
+        SnmpV2cClientSocket("127.0.0.1:100000", "public", 0, 0, 0, 0)
 
 
 def test_v1_set_tos() -> None:
-    SnmpV1ClientSocket("127.0.0.1:161", "public", 10, 0, 0)
+    SnmpV1ClientSocket("127.0.0.1:161", "public", 10, 0, 0, 0)
 
 
 def test_v2c_set_tos() -> None:
-    SnmpV2cClientSocket("127.0.0.1:161", "public", 10, 0, 0)
+    SnmpV2cClientSocket("127.0.0.1:161", "public", 10, 0, 0, 0)
 
 
 def test_v1_get_fd() -> None:
-    SnmpV1ClientSocket("127.0.0.1:161", "public", 0, 0, 0).get_fd()
+    SnmpV1ClientSocket("127.0.0.1:161", "public", 0, 0, 0, 0).get_fd()
 
 
 def test_v2c_get_fd() -> None:
-    SnmpV2cClientSocket("127.0.0.1:161", "public", 0, 0, 0).get_fd()
+    SnmpV2cClientSocket("127.0.0.1:161", "public", 0, 0, 0, 0).get_fd()
