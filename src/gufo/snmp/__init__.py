fe3ff7fd9ead3ee1179de58249eacef47c277cbb
Dmitry Volodin <dv@nocproject.org>
2024-02-28 11:54:26 +0100

    0.5.1

--- src/gufo/snmp/__init__.py
+++ src/gufo/snmp/__init__.py
@@ -18,7 +18,7 @@ from .typing import ValueType
 from .user import Aes128Key, DesKey, Md5Key, Sha1Key, User
 from .version import SnmpVersion
 
-__version__: str = "0.5.0"
+__version__: str = "0.5.1"
 __all__ = [
     "__version__",
     "Aes128Key",
